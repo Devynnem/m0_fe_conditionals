313003c7466cc626fcfd12832058cbf60420cf57
Devynne Marshall <devynnem@gmail.com>
2022-12-06 19:46:06 -0700

    show changes made in comparisons.js and ifStatements.js

--- comparisons.js
+++ comparisons.js
@@ -9,7 +9,7 @@
 
 // -------------------
 // PART 1: Comparing variables
-// -------------------
+// ------------------- 
 
 var numberTeachers = 4
 var numberStudents = 20
@@ -21,24 +21,31 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
+console.log("Is numberTeachers less than numberStudents?", numberTeachers < numberStudents)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
+console.log("Is numberTeachers strictly equal to stringTeachers?", numberTeachers === stringTeachers)
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
+console.log("Is numberTeachers not equal to numberStudents?", numberTeachers !== numberStudents)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
+console.log("Is numberStudents greater than or equal to 20?", numberStudents >= 20)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
+console.log("Is numberStudents greater than or equal to 21?", numberStudents >= 21)
 // this should log: false
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
+console.log("Is numberStudents less than or equal to 20?", numberStudents <= 20)
 // this should log: true
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
+console.log("Is numberStudents less than or equal to 21?", numberStudents <= 21)
 // this should log: true
 
 
@@ -52,21 +59,26 @@ console.log("Is numberTeachers greater than numberStudents?", numberTeachers > n
 // Make sure YOU can explain it that way!
 
 console.log(4 < 9);
-// YOU DO: Explain.
+// YOU DO: Explain. 
+// Is 4 less than 9? true
+
 
 var books = 3;
 console.log(4 < books);
-// YOU DO: Explain.
+// YOU DO: Explain. 
+// There are 3 books. Is 4 less than the amount of books? false
 
 var friends = 6;
 var siblings = 2;
 console.log(friends > siblings);
-// YOU DO: Explain.
+// YOU DO: Explain. 
+// There are 6 friends. There are 2 siblings. Is the amount of friends greater than the amount of siblings? true
 
 var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
-// YOU DO: Explain.
+// YOU DO: Explain. 
+// There are 9 attendees. There are 8 meals. Is the amount of attendees not equal to the amount of meals? true
 
 
 // #-------------------
@@ -89,15 +101,17 @@ var age = 1;
 
 // YOU DO:
 // Determine if the dog loves to play and loves treats
+console.log(lovesToPlay && lovesTreats);
 
 
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark);
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark);
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age < 2);
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: It evaluated to true, because the puppies age is still one and I asked if the dog was younger than 2.
--- ifStatements.js
+++ ifStatements.js
@@ -11,7 +11,7 @@
   // if it is snowy, print "coat"
   // if it is icy, print "yak traks"
 
-var weather = "sunny"
+var weather = "snowy"
 
 if (weather === "sunny") {
   console.log("sunscreen");
@@ -42,10 +42,12 @@ if (weather === "sunny") {
 // Experiment with manipulating the value held within numQuarters
 // to make sure both conditions can be achieved.
 
-var numQuarters = 0
-
+var numQuarters = 1
+if (numQuarters === 2) {
 console.log("I have enough money for a gumball");
+} else if (numQuarters === 1) {
 console.log("I don't have enough money for a gumball");
+}
 
 
 // #####################
@@ -62,6 +64,9 @@ console.log("I don't have enough money for a gumball");
 // Experiment with manipulating the value held within both variables
 // to make sure all above conditions output what you expect.
 
-var cupsOfFlour = 1;
+var cupsOfFlour = 3;
 var hasSauce = true;
 
+if (cupsOfFlour === 3 && hasSauce === true) {
+  console.log("I can make pizza")
+}
